54bead5ea66b682e46b2115f823219c5a18e9130
Hecoal <hectorcortez_2002@hotmail.com>
2025-01-16 15:09:22 -0600

    Added update async operation

--- WebDiaryAPI/Controllers/DiaryEntriesController.cs
+++ WebDiaryAPI/Controllers/DiaryEntriesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using WebDiaryAPI.Data;
 using WebDiaryAPI.Models;
 
@@ -18,15 +19,15 @@ namespace WebDiaryAPI.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<DiaryEntry>>> GetDiaryEntries() 
-        { 
-            return  await _context.DiaryEntries.ToListAsync();
+        public async Task<ActionResult<IEnumerable<DiaryEntry>>> GetDiaryEntries()
+        {
+            return await _context.DiaryEntries.ToListAsync();
         }
 
         [HttpGet("{id}")]
-        public async Task<ActionResult<DiaryEntry>>GetDiaryEntry(int id)
+        public async Task<ActionResult<DiaryEntry>> GetDiaryEntry(int id)
         {
-            var diaryEntry =  await _context.DiaryEntries.FindAsync(id);
+            var diaryEntry = await _context.DiaryEntries.FindAsync(id);
 
             if (diaryEntry == null)
             {
@@ -36,7 +37,7 @@ namespace WebDiaryAPI.Controllers
         }
 
         [HttpPost]
-        public async Task <ActionResult<DiaryEntry>>PostDiaryEntry(DiaryEntry diaryEntry)
+        public async Task<ActionResult<DiaryEntry>> PostDiaryEntry(DiaryEntry diaryEntry)
         {
             diaryEntry.Id = 0;
             _context.DiaryEntries.Add(diaryEntry);
@@ -45,5 +46,39 @@ namespace WebDiaryAPI.Controllers
             var resourceUrl = Url.Action(nameof(DiaryEntry), new { id = diaryEntry.Id });
             return Created(resourceUrl, diaryEntry);
         }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDiaryEntry(int id, [FromBody] DiaryEntry diaryEntry)
+        {
+            //The id from Url and id from body doesnt match
+            if (id != diaryEntry.Id)
+            {
+                return BadRequest();
+            }
+            _context.Entry(diaryEntry).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DiaryEntryExists(id))
+                {
+                    return BadRequest();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        private bool DiaryEntryExists(int id)
+        {
+            return _context.DiaryEntries.Any(e => e.Id == id);
+        }
+
     }
 }
